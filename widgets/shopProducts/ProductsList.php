8ec0d883974d3c7043dc0ad2a55df348d48548b7
Alex Kostin <web-mayak@mail.ru>
2020-03-15 17:46:32 +1000

    функционал сортировки каталога по цене

--- widgets/shopProducts/ProductsList.php
+++ widgets/shopProducts/ProductsList.php
@@ -22,4 +22,43 @@ class ProductsList extends ProductsBaseList
             'class' => ['pagination justify-content-center align-items-center']
         ]
     ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init() : void
+    {
+        $this->layout = strtr(
+            '{sort}<div class="row">{items}</div>{pager}', [
+            '{sort}' => $this->sortLinksWidget(),
+        ]);
+        parent::init();
+    }
+
+    private function sortLinksWidget() : string
+    {
+        $pathInfo = Yii::$app->request->pathInfo;
+        $currentSort = Yii::$app->request->get('sort');
+        $iconSortAsc = Html::tag('i', '', ['class' => 'fa fa-sort-amount-asc']);
+        $iconSortDesc = Html::tag('i', '', ['class' => 'fa fa-sort-amount-desc']);
+        return Html::tag('div',
+            'Сортировка по цене: '
+            . Html::a(
+                "дешевые {$iconSortAsc}",
+                ["/{$pathInfo}", 'sort' => 'price'],
+                ['class' => 'btn btn-xs ' . ($currentSort == 'price' ? 'btn-primary' : 'btn-link')]
+            )
+            . Html::a(
+                "дорогие {$iconSortDesc}",
+                ["/{$pathInfo}", 'sort' => '-price'],
+                ['class' => 'btn btn-xs ' . ($currentSort == '-price' ? 'btn-primary' : 'btn-link')]
+            )
+            . ($currentSort ? Html::a(
+                Html::tag('i', '', ['class' => 'fa fa-close']),
+                ["/{$pathInfo}"],
+                ['class' => 'btn btn-xs btn-link']
+            ) : ''),
+            ['class' => 'products-list-sort text-right']
+        );
+    }
 }
