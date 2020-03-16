8484130322e694761462c079f305dd87fc32fa1c
Alex Kostin <web-mayak@mail.ru>
2020-03-16 23:26:52 +1000

    добавил в меню вывод еще двух уровней вложенности категорий

--- views/_catalog-menu.php
+++ views/_catalog-menu.php
@@ -9,6 +9,24 @@
                             <?php foreach ($lvl2cats as $lvl2cat): ?>
                                 <li>
                                     <a href="<?= $lvl2cat->present()->getUrl() ?>"><?= $lvl2cat->name ?></a>
+                                    <?php if ($lv3categories = $lvl2cat->getChildren()->andWhere(['status' => 1])->all()): ?>
+                                    <ul>
+                                        <?php foreach ($lv3categories as $lvl3cat): ?>
+                                            <li>
+                                                <a href="<?= $lvl3cat->present()->getUrl() ?>" class="slim-link"><?= $lvl3cat->name ?></a>
+                                                <?php if ($lv4categories = $lvl3cat->getChildren()->andWhere(['status' => 1])->all()): ?>
+                                                <ul>
+                                                    <?php foreach ($lv4categories as $lvl4cat): ?>
+                                                        <li>
+                                                            <a href="<?= $lvl4cat->present()->getUrl() ?>" class="slim-link"><?= $lvl4cat->name ?></a>
+                                                        </li>
+                                                    <?php endforeach; ?>
+                                                </ul>
+                                                <?php endif; ?>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                    <?php endif; ?>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
